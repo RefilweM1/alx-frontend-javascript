3b69f8ac0908ff2d3b0ac9e1e8c430dc3d4328fc
RefilweM1 <Refilwemokwene908@gmail.com>
2024-05-29 17:42:32 +0000

    Updated

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,8 @@
 function signUpUser(firstName, lastName) {
-	return Promise.resolve({
-		firstName,
-		lastName,
-	});
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
 }
+
 export default signUpUser;
